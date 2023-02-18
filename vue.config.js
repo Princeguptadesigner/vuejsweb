67f082971a7aeda65c7d8917ed15755033f1c79d
Prince Gupta <prince@pixpa.com>
2023-02-18 10:48:06 +0530

    wip

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 module.exports = {
-  publicPath: "/vuejsweb/"
+  publicPath: process.env.NODE_ENV === "production" ? "/vuejsweb/" : "/",
 };
